fa914a8549fbd9c5c003cc5e01a2dfbdd1ccb924
Pierre <pierre.fouillet@yahoo.fr>
2016-11-29 14:44:58 +0100

    Mise à jour de la classe Joueur

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,5 +1,51 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Created by Pierre on 29/11/2016.
  */
 public class Joueur {
+
+    private Socket socketClient;
+    private ObjectInputStream objectInputStream;
+    private ObjectOutputStream objectOutputStream;
+    private Scanner sc = new Scanner(System.in);
+
+    public Joueur() throws IOException {
+        socketClient = new Socket(InetAddress.getLocalHost(),8003);
+        System.out.println("Demande de connexion au serveur");
+
+        //lire
+        objectInputStream = new ObjectInputStream(socketClient.getInputStream());
+
+        //ecrire
+        objectOutputStream = new ObjectOutputStream(socketClient.getOutputStream());
+        objectOutputStream.flush();
+
+    }
+
+    public void run() throws IOException {
+        //System.out.println("***hello Serveur***");
+        String message ="";
+        while (true) {
+            //Recupère le message du serveur
+            message=objectInputStream.readUTF();
+            //lis un string dans un format UTF
+            System.out.println("Le serveur dit :" + message);
+            System.out.println("Saisir une lettre :");
+            objectOutputStream.writeUTF(sc.nextLine());
+            objectOutputStream.flush();
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Joueur  j = new Joueur();
+        j.run();
+    }
+
 }

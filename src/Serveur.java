505ab9d7b8636080d302252d5c4b08eb03602384
Pascale <pascale.fauvy@gmail.com>
2016-11-29 14:59:38 +0100

    Création classe Serveur

--- src/Serveur.java
+++ src/Serveur.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
@@ -14,10 +15,32 @@ public class Serveur {
     private ObjectOutputStream objectOutputStream;
     private Scanner sc = new Scanner(System.in);
 
-public Serveur(){
+public Serveur() throws IOException {
+
+    socketServer = new ServerSocket(8003);
+    System.out.println("Le serveur est à l'écoute du port"+socketServer.getLocalPort());
+    connection = socketServer.accept();
+    System.out.println("Un joueur s'est connecté");
+    objectOutputStream = new ObjectOutputStream(connection.getOutputStream());
+    objectOutputStream.flush();
+    objectInputStream = new ObjectInputStream(connection.getInputStream());
+    }
+
+    public void run() throws IOException {
+        char lettre;
+        int coupRestant=10;
+
+
+        objectOutputStream.writeUTF("Bienvenue dans le Pendu");
+        objectOutputStream.flush();
+
+    while(coupRestant<=10){
+        coupRestant--;
+        System.out.println("Il vous reste"+coupRestant+"à jouer");
 
 
 
+    }
     }
     public static void main(String[] args) {
         System.out.println("Bienvenue");
